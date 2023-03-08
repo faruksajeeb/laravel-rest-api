98a17654c3895d18bd6762fce80991a087cf26b7
Omar Faruk <ofsajeeb@gmail.com>
2023-03-08 15:50:38 +0600

    add login,logout,register

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentClassController;
 
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('register', 'AuthController@register');
+
+});
+
+
 Route::ApiResource('/student_classes',StudentClassController::class);
